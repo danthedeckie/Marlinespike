412f3745686f04a506f240bd3b042c0ba83962b3
Daniel Fairhead <danthedeckie@gmail.com>
2012-04-20 07:53:30 +0100

    ideas

--- ideas.py
+++ ideas.py
@@ -1,5 +1,16 @@
 import re
 
+longer_text= \
+"""<html> 
+blah blah blah 
+<% omnitube url="video.html" size="600" %> and then
+some more <% omnitube url="here.html" size="900" %></html>"""
+
 text="<% omnitube url='blah.html' width='600' %>"
-re.findall("\s(?P<key>\S*)=[\"'](?P<val>[^\"']+)",text)
+
+def make_tag_regex(tag):
+    tag_contents_regex = re.compile("<%\s*"+tag+"(.*)%>")
+
+inside_tag_regex = re.compile("\s(?P<key>\S*)=[\"'](?P<val>[^\"']+)")
+re.findall(inside_tag_regex,text)
 
